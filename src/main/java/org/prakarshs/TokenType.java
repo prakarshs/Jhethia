75bf867d90d82a27235d9b14166f08873a698849
jhethia <jhethia@gmail.com>
2024-03-19 14:16:07 +0530

    added tokens for lexical analysis

---
+++ src/main/java/org/prakarshs/TokenType.java
@@ -0,0 +1,25 @@
+package org.prakarshs;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+//  Lexeme types with matching regex expression
+@Getter
+@RequiredArgsConstructor
+public enum TokenType {
+
+    Comment("\\#.*"),
+    LineBreak("[\\n\\r]"),
+    Whitespace("[\\s\\t]"),
+    Keyword("(agar|yafir|ya|khatam|dekhiye_baapuji|lijiye_baapuji|kilass|kaam|wapas|ghumaghum|andar|by|todiye|agla|assert|raise|ishtart|rescue|ensure)(?=\\s|$)(?!_)"),
+    GroupDivider("(\\[|\\]|\\,|\\{|}|\\.{2}|(\\:(?!\\:)))"),
+    Logical("(sahi_baat_hai|galat_baat_hai)(?=\\s|$)(?!_)"),
+    Numeric("([-]?(?=[.]?[0-9])[0-9]*(?![.]{2})[.]?[0-9]*)"),
+    Null("(khaali)(?=,|\\s|$)(?!_)"),
+    This("(ye)(?=,|\\s|$)(?!_)"),
+    Text("\"([^\"]*)\""),
+    Operator("(\\+|-|\\*{1,2}|/{1,2}|%|>=?|<=|<{1,2}|={1,2}|!=|!|:{2}\\s+naya|:{2}|\\(|\\)|(naya|aur|yatoh|as|is)(?=\\s|$)(?!_))"),
+    Variable("[a-zA-Z_]+[a-zA-Z0-9_]*");
+
+    private final String regex;
+}

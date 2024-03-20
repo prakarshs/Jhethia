33ed7aa5a63788932b752ccbe6a4fe7d59e2b2bc
jhethia <jhethia@gmail.com>
2024-03-20 15:11:57 +0530

    added equals relational operator

---
+++ src/main/java/org/prakarshs/Syntax/Expressions/Operators/EqualsOperator.java
@@ -0,0 +1,25 @@
+package org.prakarshs.Syntax.Expressions.Operators;
+
+import org.prakarshs.Syntax.Expression;
+import org.prakarshs.Syntax.Literals.Literal;
+import org.prakarshs.Syntax.Literals.LogicalLiteral;
+
+import java.util.Objects;
+
+public class EqualsOperator extends BinaryOperator{
+    public EqualsOperator(Expression left, Expression right) {
+        super(left, right);
+    }
+
+    @Override
+    public Literal<?> calc(Literal<?> left, Literal<?> right) {
+            boolean result;
+            if (Objects.equals(left.getClass(), right.getClass())) {
+                result = ((Comparable) left.getLiteral()).compareTo(right.getLiteral()) == 0;
+            } else {
+                result = ((Comparable) left.toString()).compareTo(right.toString()) == 0;
+            }
+            return new LogicalLiteral(result);
+        }
+    }
+}

7dbfde6fe13101b203123dca28a9b249ff552fc9
jhethia <jhethia@gmail.com>
2024-04-12 10:28:10 +0530

    added setValue in Variable

--- src/main/java/org/prakarshs/Syntax/Expressions/Variable.java
+++ src/main/java/org/prakarshs/Syntax/Expressions/Variable.java
@@ -4,12 +4,14 @@ import lombok.Data;
 import org.prakarshs.Syntax.Literals.Literal;
 import org.prakarshs.Syntax.Literals.TextLiteral;
 
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 @Data
 public class Variable implements Expression {
     private final String variableName;
     private final Function<String, Literal<?>> variableLiteral;
+    private final BiConsumer<String, Literal<?>> setter;
 
     @Override
     public Literal<?> evaluate() {
@@ -20,4 +22,7 @@ public class Variable implements Expression {
         return value;
     }
 
+    public void setValue(Literal<?> literal) {
+        setter.accept(variableName,literal);
+    }
 }
